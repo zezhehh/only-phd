595ad28345e6c85569ceaac218a2b760f84d303e
zezhehh <zezhehh@outlook.com>
2023-06-11 14:26:31 +0200

    under construction page

---
+++ index.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    colors: {
+      primary: "var(--primary-color)",
+      secondary: "var(--secondary-color)",
+      neutral: "var(--neutral-color)",
+      orange: "var(--orange-color)",
+      error: "var(--error-color)",
+      black: "var(--black-color)",
+      white: "var(--white-color)",
+    },
+  },
+};
