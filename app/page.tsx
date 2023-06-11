6d6447bb017dafde1e7ce84a51693227387c449b
zezhehh <zezhehh@outlook.com>
2023-06-11 17:40:04 +0200

    add mapbox

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import App from '../components/app'
+
+export default function Home() {
+  return (
+    <main className='w-screen h-screen overflow-x-hidden'>
+      <App />
+    </main>
+  )
+}
