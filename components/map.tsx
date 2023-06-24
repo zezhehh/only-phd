abfec2eb2ce4d453f7d78810f22b1d114b4f6cb0
zezhehh <zezhehh@outlook.com>
2023-06-24 23:09:29 +0200

    Hover effect

--- components/map.tsx
+++ components/map.tsx
@@ -6,7 +6,7 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import { useAtom } from "jotai";
 import { curPlaceAtom } from "@/utils/states";
 import { useHoverDirty } from "react-use";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import mapboxSdk from "@mapbox/mapbox-sdk/services/geocoding";
 
 const Map = () => {
@@ -58,14 +58,98 @@ const Map = () => {
       zoom: 1,
       logoPosition: "bottom-right",
     });
-    map.current.on("click", (e: { point: any }) => {
-      var features = map.current.queryRenderedFeatures(e.point);
-      console.log(JSON.stringify(features, null, 2));
+    map.current.on("click", async (e: { point: any; lngLat: any }) => {
+      // var features = map.current.queryRenderedFeatures(e.point);
+      const { lng, lat } = e.lngLat;
+
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${mapboxgl.accessToken}`
+      );
+      const data = await response.json();
+
+      // Extract country details
+      const countryFeatures = data.features.filter((feature: any) =>
+        feature.place_type.includes("country")
+      );
+
+      if (countryFeatures.length > 0) {
+        const countryName = countryFeatures[0].text;
+        const countryCode = countryFeatures[0].properties.short_code;
+
+        console.log("Country:", countryName, "Code:", countryCode);
+      }
+    });
+
+    map.current.on("load", () => {
+      // https://stackoverflow.com/questions/67906883/hover-effect-for-countries
+      map.current.addSource("cbs", {
+        type: "geojson",
+        data: "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson",
+      });
+
+      map.current.addLayer({
+        id: "cf", // country-fills
+        type: "fill",
+        source: "cbs",
+        layout: {},
+        paint: {
+          "fill-color": "#3f94b8",
+          "fill-opacity": 0,
+        },
+      });
+
+      map.current.addLayer({
+        id: "cb", // country borders
+        type: "line",
+        source: "cbs",
+        layout: {},
+        paint: {
+          "line-color": "#3f94b8",
+          "line-width": 0,
+        },
+      });
+
+      map.current.addLayer({
+        id: "cfh", // country-fills-hover",
+        type: "fill",
+        source: "cbs",
+        layout: {},
+        paint: {
+          "fill-color": "#3f94b8",
+          "fill-opacity": 0.5,
+        },
+        filter: ["==", "name", ""],
+      });
+
+      map.current.on("mousemove", function (e: any) {
+        var features = map.current.queryRenderedFeatures(e.point, {
+          layers: ["cf"],
+        });
+
+        if (features.length) {
+          map.current.getCanvas().style.cursor = "pointer";
+          map.current.setFilter("cfh", [
+            "==",
+            "name",
+            features[0].properties.name,
+          ]);
+        } else {
+          map.current.setFilter("cfh", ["==", "name", ""]);
+          map.current.getCanvas().style.cursor = "";
+        }
+      });
+
+      map.current.on("mouseout", function () {
+        map.current.getCanvas().style.cursor = "auto";
+        map.current.setFilter("cfh", ["==", "name", ""]);
+      });
     });
 
     marker.current = new mapboxgl.Marker({
       color: "#e3a47a",
     });
+
+    return () => map.current.remove();
   }, []);
 
   useEffect(() => {
